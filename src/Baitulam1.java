b9b22e6a4ffe7d989c7c6ce6e28c9758998c362e
khoa <hatgiong@gmail.com>
2021-10-30 11:13:49 +0700

    Initial commit

---
+++ src/Baitulam1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Baitulam1 {
+    public static void main(String[] args) {
+        Scanner mykhoa= new Scanner(System.in);
+        System.out.println("Gõ vào tên bạn");
+
+
+    }
+}
